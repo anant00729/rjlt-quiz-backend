629ee94bceb946e82cb8568aa875044910474f2a
anant007 <anant.awasthy29@gmail.com>
2019-12-19 22:22:35 +0530

    done

--- config/database.js
+++ config/database.js
@@ -2,4 +2,7 @@ const Sequelize = require('sequelize')
 
 
 //module.exports =  new Sequelize('postgres://postgres:password@db:5432/quonquer');
-module.exports =  new Sequelize('postgres://postgres:123123123@localhost:5432/resolute');
+
+
+//module.exports =  new Sequelize('postgres://rrooztyoqrfogo:fd1725005d1b908eff379c11f22e28f46898b6e8bb6c17562a37e7de0bd8882c@ec2-54-235-167-210.compute-1.amazonaws.com:5432/d2cmqdh7db89as');
+module.exports =  new Sequelize('postgres://rrooztyoqrfogo:fd1725005d1b908eff379c11f22e28f46898b6e8bb6c17562a37e7de0bd8882c@ec2-54-235-167-210.compute-1.amazonaws.com:5432/d2cmqdh7db89as');
